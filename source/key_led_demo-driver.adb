795eeb4f822438141d3292aaf887d1ddb626eecd
Vadim Godunko <vgodunko@gmail.com>
2025-01-22 17:33:24 +0400

    Update to latest changes in ARMv7M crate.

--- source/key_led_demo-driver.adb
+++ source/key_led_demo-driver.adb
@@ -1,10 +1,10 @@
 --
---  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--  Copyright (C) 2024-2025, Vadim Godunko <vgodunko@gmail.com>
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
-with A0B.ARMv7M.CMSIS;
+with A0B.ARMv7M.Instructions;
 with A0B.STM32F401.SVD.RCC; use A0B.STM32F401.SVD.RCC;
 
 procedure Key_LED_Demo.Driver is
@@ -15,11 +15,11 @@ begin
 
    Key_LED_Demo.Initialize;
 
-   A0B.ARMv7M.CMSIS.Enable_Interrupts;
+   A0B.ARMv7M.Instructions.Enable_Interrupts;
 
    loop
       --  Switch CPU into lower power mode.
 
-      A0B.ARMv7M.CMSIS.Wait_For_Interrupt;
+      A0B.ARMv7M.Instructions.Wait_For_Interrupt;
    end loop;
 end Key_LED_Demo.Driver;
